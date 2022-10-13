4f12330ce49bf35d2b8c02cd9cfc0b40505325c5
Felix Lai <felix.lai@hotmail.com>
2022-10-13 15:12:03 -0400

    advice gen
    advice generated

--- src/components/AdviceGenerator/AdviceGenerator.jsx
+++ src/components/AdviceGenerator/AdviceGenerator.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react"
+import pauseMobile from "../../assets/images/pattern-divider-mobile.svg"
+import pauseDesktop from "../../assets/images/pattern-divider-desktop.svg"
+import dice from "../../assets/images/icon-dice.svg"
 import { adviceEndPoint } from "../../utils/adviceApi";
 
 function AdviceGenerator() {
-  const [advice, setAdvice] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [text, setText] = useState([]);
 
-  const fetchAdvice = (e) => {
-    axios
-      .get(`${adviceEndPoint}`)
-      .then((response) => {
-        setAdvice(response.advice);
-        setLoading(false);
-      })
-      .catch((error) => console.log(error));
+  const fetchAdvice = async () => {
+    const res = await fetch(`${adviceEndPoint}`);
+    const data = await res.json();
+
+    setText(data.slip);
   };
+  useEffect(() => {
+      fetchAdvice()
+  }, [])
   return (
-    <div className="advice-gen">
-      <button
-        className="advice-gen__button"
-        onChange={(e) => fetchAdvice(e.target.value)}
-      >
-        randomize
-      </button>
+    <div className="container">
+      <h1>Advice #{text.id}</h1>
+      <p>{text.advice}</p>
+
+      <picture>
+        <source media="(min-width: 768px)" srcSet={pauseDesktop} />
+        <img src={pauseMobile} alt="" />
+      </picture>
+
+      <div>
+        <button onClick={fetchAdvice}>
+          <img src={dice} alt="" />
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default AdviceGenerator;
